ec6bd7cff824b7e50b73ae06fce4204302fca6a6
ManSmooth <40175264+ManSmooth@users.noreply.github.com>
2023-08-04 21:55:17 +0700

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <nav class="flex bg-stone-700 font-serif px-4 py-2 gap-4 text-white items-baseline">
+      <RouterLink :to="{ name: 'home' }" class="text-xl font-bold">Stu<span class="text-amber-400">Connect</span></RouterLink>
+      <RouterLink :to="{ name: 'home' }">Home</RouterLink>
+    </nav>
+  </header>
+  <div class="font-serif text-white">
+    <RouterView />
+  </div>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <main>
+    <div class="bg-grey-500">
+      <p>Welcome!</p>
+    </div>
+  </main>
+</template>
