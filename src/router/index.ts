f31637ebd417ece012da8b937ee2fa6623b663af
hoshizaki <kant.cmd@gmail.com>
2023-08-17 21:23:57 +0700

    commit before merge

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import CourseView from "@/views/CourseListView.vue";
+import CourseDetail from "@/views/Course/CourseDetail.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -20,6 +22,39 @@ const router = createRouter({
           next()
         }
       }
+    },
+    {
+      path: '/course',
+      name: 'CourseList',
+      component: CourseView,
+      props: (route) => ({ page: parseInt(route.query?.page as string || '1' )}),
+      beforeEnter: (to, _ , next)  => {
+        if (
+            !to.query?.page ||
+            parseInt(to.query?.page as string) < 1 ||
+            isNaN(parseInt(to.query?.page as string))
+        ) {
+          next({ name: 'CourseList', query: { page: 1 } })
+        } else {
+          next()
+        }
+      }
+    },
+    {
+      path: '/course/:id',
+      name: 'Course Layout',
+      props: true,
+      beforeEnter: (to) => {
+        const id: number = parseInt(to.params.id as string)
+      },
+      children: [
+        {
+          path: '',
+          name: 'CourseDetail',
+          component: CourseDetail,
+          props: true
+        }
+      ]
     }
   ]
 })
---
+++ src/views/CourseListView.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import {useRouter} from "vue-router";
+
+// const router = useRouter()
+defineProps({
+  page: {
+    type: Number,
+    required: true
+  }
+})
+</script>
+
+<template>
+<h1>Hi Courses</h1>
+</template>
+
+<style scoped>
+
+</style>
