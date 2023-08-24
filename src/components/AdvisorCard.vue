ff456e99f9f0c3cdca22e1f697f4bfbf754de699
Very Smooth Man <40175264+ManSmooth@users.noreply.github.com>
2023-08-24 20:45:55 +0700

    UI Enhancements

--- src/components/AdvisorCard.vue
+++ src/components/AdvisorCard.vue
@@ -13,7 +13,7 @@ defineProps({
 <template>
   <RouterLink :to="{ name: 'advisor-detail', params: { id: advisor.id } }">
     <div
-      class="flex flex-row sm:flex-col overflow-hidden shadow-md bg-stone-800 hover:border-b-2 hover:pb-[2px] transition-all border-emerald-400 duration-100 group">
+      class="flex flex-row h-full sm:flex-col overflow-hidden shadow-md bg-stone-800 hover:border-emerald-400 transition-all border-r-2 sm:border-r-0 sm:border-b-2 border-transparent duration-100 group">
       <div>
         <img :src=advisor.image class="w-full aspect-square"
           alt="{{ advisor.prefix ? advisor.prefix + ' ': '' }}{{advisor.first_name}} {{advisor.last_name}}" />
@@ -22,8 +22,7 @@ defineProps({
         <p class="text-xs opacity-50">#{{ advisor.id }}</p>
         <p class="text-lg group-hover:text-emerald-400 transition-colors text-ellipsis">{{ advisor.prefix ? advisor.prefix
           + ' ' : '' }}{{ advisor.first_name }} {{ advisor.last_name }}</p>
-        <p class="text-sm font-light opacity-75">{{ advisor.faculty
-        }}</p>
+        <p class="text-sm font-light opacity-75">{{ advisor.faculty }}</p>
       </div>
     </div>
   </RouterLink>
--- src/components/StudentCard.vue
+++ src/components/StudentCard.vue
@@ -1,22 +1,27 @@
-<template>
-    <RouterLink :to="{ name: 'student-information', params: { id: student.id } }">
-      <div class="relative font-serif bg-stone-800 shadow-md flex flex-row gap-4 hover:border-l-4 hover:pl-[2px] transition-all border-emerald-400 duration-100 group">
-        <img :src="student.image" class="h-16 w-16 object-cover" />
-        <div class="flex flex-col py-2 justify-center">
-            <p class="text-xs opacity-50">#{{ student.id }}</p>
-            <p class="text-xl group-hover:text-emerald-400 transition-colors duration-100">{{ `${student.first_name} ${student.last_name}` }}</p>
-        </div>
-      </div>
-    </RouterLink>
-</template>
 <script setup lang="ts">
 import type { Student } from '@/types';
 import type { PropType } from 'vue';
+const intl = new Intl.DisplayNames(['en'], {type: 'region'})
 
 defineProps({
-    student: {
-        type: Object as PropType<Student>,
-        required: true
-    }
+  student: {
+    type: Object as PropType<Student>,
+    required: true
+  }
 })
+
 </script>
+
+<template>
+  <RouterLink :to="{ name: 'student-information', params: { id: student.id } }">
+    <div
+      class="relative font-serif bg-stone-800 shadow-md flex flex-row gap-4 hover:border-l-4 hover:pl-[2px] transition-all border-emerald-400 duration-100 group">
+      <img :src="student.image" class="w-24 aspect-square object-cover" />
+      <div class="flex flex-col py-2 justify-start">
+        <p class="text-xs opacity-50">#{{ student.id }}</p>
+        <p class="text-xl group-hover:text-emerald-400 transition-colors duration-100">{{ `${student.first_name}
+                  ${student.last_name}` }}</p>
+        <p class="text-sm transition-colors duration-100 opacity-75">{{ intl.of(student.nationality) }}</p>
+      </div>
+    </div>
+  </RouterLink></template>
