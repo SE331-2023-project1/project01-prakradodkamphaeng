f27804cbbb7569cec9090940340805d7cbaa5db3
Very Smooth Man <40175264+ManSmooth@users.noreply.github.com>
2023-08-24 22:25:33 +0700

    Fix image

--- src/components/AdvisorCard.vue
+++ src/components/AdvisorCard.vue
@@ -15,7 +15,7 @@ defineProps({
     <div
       class="flex flex-row h-full sm:flex-col overflow-hidden shadow-md bg-stone-800 hover:border-emerald-400 transition-all border-r-2 sm:border-r-0 sm:border-b-2 border-transparent duration-100 group">
       <div>
-        <img :src=advisor.image class="w-full aspect-square"
+        <img :src=advisor.image class="w-full aspect-square object-cover"
           alt="{{ advisor.prefix ? advisor.prefix + ' ': '' }}{{advisor.first_name}} {{advisor.last_name}}" />
       </div>
       <div class="px-6 py-4">
