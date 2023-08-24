42808eaf9792ac786254568dbf6c1a1271985df0
Very Smooth Man <40175264+ManSmooth@users.noreply.github.com>
2023-08-24 20:56:08 +0700

    Improve transitions

--- src/components/CourseCard.vue
+++ src/components/CourseCard.vue
@@ -12,8 +12,8 @@ defineProps({
 
 <template>
   <RouterLink :to="{ name: 'course-detail', params: { id: course.id } }"
-    class="relative p-6 shadow-md bg-stone-800   flex flex-col gap-2 group hover:border-l-4 border-emerald-400 transition-all">
-    <div class="group-hover:ml-[2px] flex flex-col gap-2">
+    class="relative p-6 shadow-md bg-stone-800 flex flex-col gap-2 group border-l-4 border-transparent hover:border-emerald-400 transition-all">
+    <div class="flex flex-col gap-2">
       <div class="flex flex-col md:flex-row items-center gap-2 md:gap-8">
         <p class="opacity-50">#{{ course.id }}</p>
         <p class="sm:text-2xl text-1xl font-bold text-white group-hover:text-emerald-400 transition-colors">{{
