2d25551adf205ab67e2eecec52f27ebc51c10b66
Very Smooth Man <40175264+ManSmooth@users.noreply.github.com>
2023-08-24 22:38:10 +0700

    Fix UI Clipping Oversight

--- src/views/AdvisorDetailView.vue
+++ src/views/AdvisorDetailView.vue
@@ -1,19 +1,21 @@
 <template>
     <main class="flex max-w-6xl w-full bg-stone-800 p-8" v-if="advisor">
         <div class="flex md:flex-row w-full gap-4 flex-col">
-            <div class="flex md:flex-col flex-row gap-4 flex-shrink-0">
+            <div class="flex flex-col gap-4 flex-shrink-0">
                 <p class="text-3xl opacity-75">Advisor</p>
-                <img :src="advisor.image" class="p-1 border border-white aspect-square w-48 object-cover">
-                <div class="flex flex-col flex-1">
-                    <RouterLink :to="{ name: 'advisor-information' }"
-                        class="[&.router-link-exact-active]:border-emerald-400 text-lg [&.router-link-exact-active]:text-emerald-400 px-2 py-1 border-l-2 hover:border-l-4 transition-all">
-                        Information</RouterLink>
-                    <RouterLink :to="{ name: 'advisor-student' }"
-                        class="[&.router-link-exact-active]:border-emerald-400 text-lg [&.router-link-exact-active]:text-emerald-400 px-2 py-1 border-l-2 hover:border-l-4 transition-all">
-                        Advisee</RouterLink>
-                    <RouterLink :to="{ name: 'advisor-courses' }"
-                        class="[&.router-link-exact-active]:border-emerald-400 text-lg [&.router-link-exact-active]:text-emerald-400 px-2 py-1 border-l-2 hover:border-l-4 transition-all">
-                        Courses</RouterLink>
+                <div class="flex flex-row md:flex-col gap-4 ">
+                    <img :src="advisor.image" class="p-1 border border-white aspect-square w-48 object-cover">
+                    <div class="flex flex-col flex-1">
+                        <RouterLink :to="{ name: 'advisor-information' }"
+                            class="[&.router-link-exact-active]:border-emerald-400 text-lg [&.router-link-exact-active]:text-emerald-400 px-2 py-1 border-l-2 hover:border-l-4 transition-all">
+                            Information</RouterLink>
+                        <RouterLink :to="{ name: 'advisor-student' }"
+                            class="[&.router-link-exact-active]:border-emerald-400 text-lg [&.router-link-exact-active]:text-emerald-400 px-2 py-1 border-l-2 hover:border-l-4 transition-all">
+                            Advisee</RouterLink>
+                        <RouterLink :to="{ name: 'advisor-courses' }"
+                            class="[&.router-link-exact-active]:border-emerald-400 text-lg [&.router-link-exact-active]:text-emerald-400 px-2 py-1 border-l-2 hover:border-l-4 transition-all">
+                            Courses</RouterLink>
+                    </div>
                 </div>
             </div>
             <div class="flex flex-col gap-4 flex-1">
--- src/views/StudentDetailView.vue
+++ src/views/StudentDetailView.vue
@@ -1,19 +1,21 @@
 <template>
     <main class="flex max-w-6xl w-full bg-stone-800 p-8" v-if="student">
         <div class="flex md:flex-row w-full gap-4 flex-col">
-            <div class="flex md:flex-col flex-row gap-4 flex-shrink-0">
+            <div class="flex flex-col gap-4 flex-shrink-0">
                 <p class="text-3xl opacity-75">Student</p>
-                <img :src="student.image" class="p-1 border border-white aspect-square w-48 object-cover">
-                <div class="flex flex-col flex-1">
-                    <RouterLink :to="{ name: 'student-information' }"
-                        class="[&.router-link-exact-active]:border-emerald-400 text-lg [&.router-link-exact-active]:text-emerald-400 px-2 py-1 border-l-2 hover:border-l-4 transition-all">
-                        Information</RouterLink>
-                    <RouterLink :to="{ name: 'student-courses' }"
-                        class="[&.router-link-exact-active]:border-emerald-400 text-lg [&.router-link-exact-active]:text-emerald-400 px-2 py-1 border-l-2 hover:border-l-4 transition-all">
-                        Courses</RouterLink>
-                    <RouterLink :to="{ name: 'student-comments' }"
-                        class="[&.router-link-exact-active]:border-emerald-400 text-lg [&.router-link-exact-active]:text-emerald-400 px-2 py-1 border-l-2 hover:border-l-4 transition-all">
-                        Comments</RouterLink>
+                <div class="flex flex-row md:flex-col gap-4 ">
+                    <img :src="student.image" class="p-1 border border-white aspect-square w-48 object-cover">
+                    <div class="flex flex-col flex-1">
+                        <RouterLink :to="{ name: 'student-information' }"
+                            class="[&.router-link-exact-active]:border-emerald-400 text-lg [&.router-link-exact-active]:text-emerald-400 px-2 py-1 border-l-2 hover:border-l-4 transition-all">
+                            Information</RouterLink>
+                        <RouterLink :to="{ name: 'student-courses' }"
+                            class="[&.router-link-exact-active]:border-emerald-400 text-lg [&.router-link-exact-active]:text-emerald-400 px-2 py-1 border-l-2 hover:border-l-4 transition-all">
+                            Courses</RouterLink>
+                        <RouterLink :to="{ name: 'student-comments' }"
+                            class="[&.router-link-exact-active]:border-emerald-400 text-lg [&.router-link-exact-active]:text-emerald-400 px-2 py-1 border-l-2 hover:border-l-4 transition-all">
+                            Comments</RouterLink>
+                    </div>
                 </div>
             </div>
             <div class="flex flex-col gap-4 flex-1">
